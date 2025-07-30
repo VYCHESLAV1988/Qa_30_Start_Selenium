0622654857de51dbb9c1e4c1ddf352ce1aaba7ca
VYACHESLAV VASILTSOV <svngdv@gmail.com>
2025-07-30 22:47:42 +0300

    HW 1

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -1,9 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.net.SocketOption;
+import java.util.List;
 
 public class HomeWork {
 
@@ -20,18 +24,19 @@ public class HomeWork {
     @Test
     public void cssSelectors(){
         //By tagName
-
-        //html
-
+        WebElement html = wd.findElement(By.tagName("html")); //html
+        WebElement body = wd.findElement(By.tagName("body")); //body
+        WebElement div =  wd.findElement(By.tagName("div"));  //div
+        WebElement h1 = wd.findElement(By.tagName("h1")); //h1
+        WebElement link = wd.findElement(By.tagName("a")); //a == list
+        List<WebElement> links = wd.findElements(By.tagName("a"));
+        WebElement form = wd.findElement(By.tagName("form"));  //form
+        WebElement email = wd.findElement(By.tagName("input")); //input
+        WebElement password = wd.findElement(By.tagName("input"));
+        WebElement login = wd.findElement(By.tagName("button")); //button
+        WebElement Registration = wd.findElement(By.tagName("button"));
         
-        //body
-        //div
-        //h1
-        //href
-        //a == list
-        //form
-        //input
-        //button
+
 
 
 
@@ -40,7 +45,7 @@ public class HomeWork {
     @AfterClass
     public void tearDown(){
          //wd.quit();  // closed  full opened tabs
-        //wd.close(); // closed one tab
+        wd.close(); // closed one tab
     }
 
 }

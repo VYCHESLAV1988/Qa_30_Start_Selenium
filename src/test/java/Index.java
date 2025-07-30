629167acf7f93ed8114b2955f3e71cc6f706290e
VYACHESLAV VASILTSOV <svngdv@gmail.com>
2025-07-30 21:38:23 +0300

    Created class Index

---
+++ src/test/java/Index.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Index {
+
+    WebDriver wd;
+
+    @BeforeClass
+    public void setUp(){
+        wd = new ChromeDriver();
+        wd.get("file:C://Tools\\21.index.html");
+    }
+
+    @Test
+    public void cssLocators(){
+        //by tagName
+       WebElement el = wd.findElement(By.tagName("button"));
+       WebElement input = wd.findElement(By.tagName("input"));
+       List<WebElement> list = wd.findElements(By.tagName("input"));
+    }
+
+    
+
+}

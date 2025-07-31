1f1899806b1ce9a50263c4136577b03bbd406dd3
VYCHESLAV1988 <svngdv@gmail.com>
2025-07-31 15:32:24 +0300

    Index

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -25,16 +25,26 @@ public class HomeWork {
     public void cssSelectors(){
         //By tagName
         WebElement html = wd.findElement(By.tagName("html")); //html
+        WebElement html1 = wd.findElement(By.cssSelector("html"));
         WebElement body = wd.findElement(By.tagName("body")); //body
+        WebElement body1 = wd.findElement(By.cssSelector("body"));
         WebElement div =  wd.findElement(By.tagName("div"));  //div
+        WebElement div2; wd.findElement(By.cssSelector("div"));
         WebElement h1 = wd.findElement(By.tagName("h1")); //h1
+        WebElement h2 = wd.findElement(By.cssSelector("h1"));
         WebElement link = wd.findElement(By.tagName("a")); //a == list
+        WebElement Link1 = wd.findElement(By.cssSelector("a"));
         List<WebElement> links = wd.findElements(By.tagName("a"));
         WebElement form = wd.findElement(By.tagName("form"));  //form
+        WebElement form1 = wd.findElement(By.cssSelector("form"));
         WebElement email = wd.findElement(By.tagName("input")); //input
+        WebElement email1 = wd.findElement(By.cssSelector("input"));
         WebElement password = wd.findElement(By.tagName("input"));
+        WebElement password1 = wd.findElement(By.cssSelector("input"));
         WebElement login = wd.findElement(By.tagName("button")); //button
+        WebElement login1 = wd.findElement(By.cssSelector("button"));
         WebElement Registration = wd.findElement(By.tagName("button"));
+        WebElement Registration1 = wd.findElement(By.cssSelector("button"));
         
 
 
--- src/test/java/Index.java
+++ src/test/java/Index.java
@@ -14,7 +14,7 @@ public class Index {
     @BeforeClass
     public void setUp(){
         wd = new ChromeDriver();
-        wd.get("file:C://Tools\\21.index.html");
+        wd.get("file:///C://Tools/21.index.html");
     }
 
     @Test
@@ -23,6 +23,31 @@ public class Index {
        WebElement el = wd.findElement(By.tagName("button"));
        WebElement input = wd.findElement(By.tagName("input"));
        List<WebElement> list = wd.findElements(By.tagName("input"));
+
+       //by class find Elements  .container
+        WebElement div = wd.findElement(By.className("container"));
+        WebElement div1 = wd.findElement(By.cssSelector(".container"));
+
+        List<WebElement>list1 = wd.findElements(By.className("nav-item"));
+        List<WebElement>list2 = wd.findElements(By.cssSelector(".nav-item"));
+
+        //by id find Elements #
+        WebElement nav = wd.findElement(By.id("nav"));
+        WebElement nav1 = wd.findElement(By.cssSelector("#nav"));
+
+        //by attribute find Elements [] attribute and value == [placeholder='value'] only for ccsSelector
+        WebElement inputEmail = wd.findElement(By.cssSelector("[placeholder='Type your name']"));
+        WebElement a3 = wd.findElement(By.cssSelector("[href='#item3']"));
+        WebElement inputPassword = wd.findElement(By.cssSelector("[placeholder='Type your surename']"));
+
+        //by name
+        WebElement inputS = wd.findElement(By.cssSelector("[name='surename']"));
+        WebElement inputS1 = wd.findElement(By.name("surename"));
+
+        //By.linkText / By.partialLinkText
+        WebElement a = wd.findElement(By.linkText("Item 3"));
+        WebElement a2 = wd.findElement(By.partialLinkText("m 2"));
+        
     }
 
 
